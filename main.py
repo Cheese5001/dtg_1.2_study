336954468aee57bd5d8ec02a05f13d4ec47c18f6
Cheese5001 <kea.are.cool@gmail.com>
2025-08-25 21:13:36 +1200

    Update main.py

--- main.py
+++ main.py
@@ -7,34 +7,43 @@
         # maby swap it with subject selction 
     # make end menu work
         #quiting and restartung
-    # 
+    # high scores in csv
 
 
+tectonic_questions = {
 
-geo_questions = {
+    "low viscosity lava oozing gently": "shield volcano",
+    "medium viscosity lava with repeated eruptions building a cone": "Stratovolcano",
+    "high viscosity lava with explosive eruption and collapse": "Caldera",
+
+    }
+
+coastal_questions = {
+    "convection currents in the mantle pulling plates apart": "mid-ocean ridges",
+    "convection currents in the mantle pushing one plate under another": "ocean trenches",
+    "longshore drift depositing sand beyond a headland": "spits",
+    "waves piling up sand offshore into a ridge": "sand bar",
+#   "waves eroding cliffs by air pressure in cracks": "Hydraulic action features (e.g. notches, caves)",
+    "destructive waves with strong swash and weak backwash": "Beach erosion",
+    "constructive waves with weak swash and strong backwash": "Beach deposition",
+    }
+
+glacial_questions = {
+    "snow compacting in mountain hollows and eroding into a deep hole": "corrie lake",
+    "glacier carving through a valley": "u-shaped valley",
+    "a glacier eroding below sea level and flooding with seawater": "fjord",
+    "glacier pushing rock debris to its sides": "lateral moraine",
+    "glacier depositing rock debris at its end": "terminal moraine",
+}
+
+fluvial_questions = {
     "vertical erosion of a river bed": "v-shaped valleys",
     "erosion at different rock hardness in a river's path": "waterfalls",
     "outside bend erosion and inside bend deposition": "meanders",
     "a river bend being cut off": "ox-bow lake",
     "river flooding and deposition of heavy sediment along the banks": "levees",
     "finer sediment spread across valley during floods": "flood plain",
-    "low viscosity lava oozing gently": "shield volcano",
-    "medium viscosity lava with repeated eruptions building a cone": "Stratovolcano",
-    "high viscosity lava with explosive eruption and collapse": "Caldera",
-    "convection currents in the mantle pulling plates apart": "Mid-ocean ridges",
-    "convection currents in the mantle pushing one plate under another": "Ocean trenches",
-    "longshore drift depositing sand beyond a headland": "Spits",
-    "waves piling up sand offshore into a ridge": "Sand bar",
-    "snow compacting in mountain hollows and eroding into a deep hole": "Corrie lake",
-    "glacier carving through a valley": "U-shaped valley",
-    "a glacier eroding below sea level and flooding with seawater": "Fjord",
-    "glacier pushing rock debris to its sides": "Lateral moraine",
-    "glacier depositing rock debris at its end": "Terminal moraine",
-#   "waves eroding cliffs by air pressure in cracks": "Hydraulic action features (e.g. notches, caves)",
-    "destructive waves with strong swash and weak backwash": "Beach erosion",
-    "constructive waves with weak swash and strong backwash": "Beach deposition",
-    }
-
+}
 
 
 def start_menu() -> str:
@@ -67,47 +76,62 @@ def choose_mode()-> str:
         mode = "quiz"
     return mode
 
-def difficulty_selection():
+def process_selection():
 
-    difficulty = input("Select difficlty: Easy (E), Medium(M), Hard (H) >> ").lower()
+    process = input("Select processes: Coastal (C), Fluvial (F), Glacial (G), Tectonic (T) >> ").lower()
 
-    if difficulty == "easy" or difficulty == "e":
-        difficulty = "easy" #so messsgae is correct
-    elif difficulty == "medium" or difficulty == "m":
-        difficulty = "medium"
-    elif difficulty == "hard" or difficulty == "h":
-        difficulty = "hard"
+    if process == "coastal" or process == "c":
+        process = "coastal" # so it writes it properly
+        questions = coastal_questions
+    elif process == "fluvial" or process == "f":
+        process = "fluvial"
+        questions = fluvial_questions
+    elif process == "glacial" or process == "g":
+        process = "glacial"
+        questions = glacial_questions
+    elif process == "tectonic" or process == "t":
+        process = "tectonic"
+        questions = tectonic_questions
     else:
-        print("Invalid difficulty selected, defaulting to Easy mode.")
-        difficulty = "easy"
+        print("Invalid process selected, defaulting to Coastal mode.")
+        process = "fluvial"
+        questions = fluvial_questions
 
-    print(f"Difficulty set to: {difficulty}")
-    return difficulty
+    print(f"Process set to: {process}")
+    return process, questions
 
 def study_mode():
-    difficulty = difficulty_selection()
+    process = process_selection()
     pass
 
 
 def quiz_mode():
-    #difficulty = difficulty_selection()
+
+
+    process, questions = process_selection()
+
+
     score = 0
-    for key in geo_questions.keys():
+    for key in questions.keys():
         print(f"What geographical feature is formed through {key}?")
         response = input("> ")
-        if geo_questions[key].lower() in response.lower():#so if responce contains -- not just is
+        if questions[key].lower() in response.lower():#so if responce contains, not just is -- so both waterfall and waterfalls
             print("Correct!")
             score += 1
+        # if close, give a hint
+        # just give a hint anyway
+        # give a few tries
         else:
-            print(f"Wrong! The correct answer is: {geo_questions[key].upper()}")
+            print(f"Wrong! The correct answer is: {questions[key].upper()}")
 
-        # score = scoring(score, difficulty) # add more factors 
+        score = scoring(score, ) # add more factors 
 
-        # return score
+        return score
 
 
 def scoring():
-    pass
+    score = 1
+    return score
 
 def end_menu():
     pass
@@ -117,11 +141,11 @@ def main():
     mode = choose_mode()
 
     if mode == "study":
-        study_mode()
+        score = study_mode()
     elif mode == "quiz":
-        quiz_mode()
+        score = quiz_mode()
 
-    end_menu()
+    end_menu(user_name, score)
 
 
 quiz_mode()
